01d8fc3cb1ca6873e8b8c0cd7c60ba902608e1e5
François Févotte <fevotte@gmail.com>
2018-04-12 09:40:38 +0200

    Mise à jour biblio: papier PDSA

---
+++ env.el
@@ -0,0 +1,5 @@
+(progn
+  (setenv "PATH"
+          (concat "/home/francois/.gem/ruby/2.3.0/bin:" (getenv "PATH")))
+  (setenv "PATH"
+          (concat "/home/francois/projets/git/www/bibtex2html-1.98-linux/:" (getenv "PATH"))))
